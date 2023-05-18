ca54b50540fdd95c5d84eff75cb9a65cb0db14e9
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2023-05-18 23:16:51 +0200

    Load data form CSV, first question to user

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Prolog_Recommendation_System
+# Film-Recommendation-System
+
+Run the program in `swipl`. 
+
+To ensure diacritic characters are printed (foreign film titles), enter: 
+```
+set_prolog_flag(encoding, utf8).
+```
+
+When starting for the first time: 
+```
+?- [recommend]. 
+?- startinit.   
+```
+
+After `startinit` has been called: 
+```
+?- start. 
+```
---
+++ gendb.pl
@@ -0,0 +1,50 @@
+:- use_module(library(persistency)).
+:- persistent(db(id:atom, property:atom, value:atom)).
+
+% generate product knowledge base from provided csv file
+
+initdb :-
+    open('product_list.csv', read, Stream),
+    parseProducts(Stream).
+    close(Stream).
+
+% do nothing if at the end of stream
+parseProducts(Stream) :-
+    at_end_of_stream(Stream),
+    writeln("Reached end of stream"). 
+
+parseProducts(Stream) :-
+    \+ at_end_of_stream(Stream),
+    csv_options(Options, [arity(7)]),
+    csv_read_row(Stream, Row, Options),
+    addInfo(Row),
+    parseProducts(Stream).
+
+% add info contained in Row into knowledge base
+addInfo(Row) :-
+    % convert terms into lists
+    Row =.. [_, ID, Category, Status, Name, Price, Rating, QtyInStock],
+    addTitle(ID, Category, Status, Name, Price, Rating, QtyInStock).
+
+% return true (skip film) if one of the arguments is missing from the csv file
+addTitle('',_,_,_,_,_,_).
+addTitle(_,'',_,_,_,_,_).
+addTitle(_,_,'',_,_,_,_).
+addTitle(_,_,_,'',_,_,_).
+addTitle(_,_,_,_,'',_,_).
+addTitle(_,_,_,_,_,'',_).
+addTitle(_,_,_,_,_,_,'').
+
+% skip not in stock products
+addTitle(_,_,_,_,_,_,QtyInStock) :-
+    QtyInStock > 0.
+
+
+addTitle(ID, Category, Status, Name, Price, Rating, QtyInStock) :-
+    assert( db(ID, category, Category) ),
+    assert( db(ID, status, Status) ),
+    assert( db(ID, name, Name) ),
+    assert( db(ID, price, Price) ),
+    assert( db(ID, rating, Rating) ),
+    assert( db(ID, qtyInStock, QtyInStock) ).
+
