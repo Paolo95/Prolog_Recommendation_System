7f31a1d34c0db0dacccadf7f1cac88f6e2de4b74
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2023-07-11 19:42:11 +0200

    Added "no user found" on main

--- prolog_recommendation_system.pl
+++ prolog_recommendation_system.pl
@@ -2,22 +2,22 @@
 
 % SEARCH HISTORY OF USERS
 
-% JOHN IS AN USER THAT SEARCHED SMARTPHONE AND TABLET
+% John is a user that searched for a smartphone and a tablet
 search_history(john, [iphone_14, galaxy_tab_a8]).
 
-% LISA IS AN USER THAT SEARCHED A TABLET
+% Lisa is a user that searched for a tablet
 search_history(lisa, [galaxy_tab_a8]).
 
-% MARK IS AN USER THAT SEARCHED A SMARTPHONE
+% Mark is a user that searched for a smartphone
 search_history(mark, [iphone_14]).
 
-% PAUL IS AN USER THAT SEARCHED TV
+% Paul is a user that searched for a TV
 search_history(paul, [ok_24GH_5023C]).
 
-% MAX IS AN USER THAT SEARCHED TV AND TABLET
+% Max is a user that searched for a TV and a tablet
 search_history(max, [ok_24GH_5023C, galaxy_tab_a8]).
 
-% CHARLES IS AN USER THAT SEARCHED SMARTPHONE AND TV
+% Charles is a user that searched for a smartphone and a TV
 search_history(charles, [iphone_14, sony_KD50X75WL]).
 
 % CATEGORIZATION OF SMARTPHONE PRODUCTS
@@ -54,6 +54,7 @@ category(galaxy_tab_s7, tablet).
 category(lenovo_p11, tablet).
 category(galaxy_tab_s6_lite, tablet).
 
+
 % CATEGORIZATION OF TV PRODUCTS
 category(ok_24GH_5023C, tv).
 category(philips_24PFS5535_12, tv).
@@ -66,6 +67,7 @@ category(sony_KD43X80L, tv).
 category(samsung_UE55AU7090UXZT, tv).
 category(sony_KD50X75WL, tv).
 category(samsung_QE50Q60BAUXZT, tv).
+
 
 % SMARTPHONE PRODUCTS' PRICE FACTS
 price(iphone_14, 859.0).
@@ -89,6 +91,7 @@ price(motorola_edge_30, 269.99).
 price(iphone_13_mini, 749.0).
 price(realme_10, 229.99).
 
+
 % TABLET PRODUCTS' PRICE FACTS
 price(galaxy_tab_a8, 229.99).
 price(galaxy_tab_a7, 149.0).
@@ -101,8 +104,8 @@ price(galaxy_tab_s7, 459.0).
 price(lenovo_p11, 299.99).
 price(galaxy_tab_s6_lite, 399.99).
 
-% TV PRODUCTS' PRICE FACTS
 
+% TV PRODUCTS' PRICE FACTS
 price(ok_24GH_5023C, 119.99).
 price(philips_24PFS5535_12, 259.99).
 price(panasonic_TX24MS350E, 229.99).
@@ -116,9 +119,14 @@ price(sony_KD50X75WL, 599.99).
 price(samsung_QE50Q60BAUXZT, 489.99).
 
 
+
 % RULES
+
+% The recommend/5 predicate recommends products to a user based on their search history,
+% price range, and category filter.
 recommend(User, MinPrice, MaxPrice, CategoryFilter, Recommendation) :-
     search_history(User, SearchHistory),
+    % Find all products that match the user's search history, category, and price range
     findall([Product, Category, Price], (
         member(Product, SearchHistory),
         category(Product, Category),
@@ -127,6 +135,7 @@ recommend(User, MinPrice, MaxPrice, CategoryFilter, Recommendation) :-
         (MaxPrice =< 0.0 ; Price =< MaxPrice),
         (CategoryFilter = 'skip' ; CategoryFilter = Category)
     ), Recommendations),
+    % Find related products that have the same category as the user's search history
     findall([RelatedProduct, Category, RelatedPrice], (
         member(Product, SearchHistory),
         category(Product, Category),
@@ -137,16 +146,21 @@ recommend(User, MinPrice, MaxPrice, CategoryFilter, Recommendation) :-
         (MaxPrice =< 0.0 ; RelatedPrice =< MaxPrice),
         (CategoryFilter = 'skip' ; CategoryFilter = Category)
     ), RelatedRecommendations),
+    % Combine and sort the recommendations
     append(Recommendations, RelatedRecommendations, AllRecommendations),
     sort(AllRecommendations, SortedRecommendations),
+    % Remove duplicate recommendations
     remove_duplicates(SortedRecommendations, UniqueRecommendations),
+    % Format the recommendations for display
     format_recommendations(UniqueRecommendations, Recommendation).
 
+% Format the recommendations as a list of formatted strings
 format_recommendations([], []).
 format_recommendations([[Product, Category, Price]|Tail], [Formatted|Result]) :-
     format(atom(Formatted), "Product Name: ~w, Category: ~w, Price: ~2f", [Product, Category, Price]),
     format_recommendations(Tail, Result).
 
+% Remove duplicate recommendations from the list
 remove_duplicates([], []).
 remove_duplicates([[Product, Category, Price]|Tail], [[Product, Category, Price]|Result]) :-
     remove_duplicates(Tail, Result).
@@ -154,35 +168,44 @@ remove_duplicates([[Product, Category, Price]|Tail], Result) :-
     member([Product, Category, Price], Tail),
     remove_duplicates(Tail, Result).
 
+% Main entry point of the program
 :- initialization(main).
 
 main :-
     write('Enter your username: '),
     read(User),
-    write('Enter the minimum price (or type "skip" to skip): '),
-    read(MinPriceInput),
-    parse_price_input(MinPriceInput, MinPrice),
-    write('Enter the maximum price (or type "skip" to skip): '),
-    read(MaxPriceInput),
-    parse_price_input(MaxPriceInput, MaxPrice),
-    write('Enter the category (or type "skip" to skip): '),
-    read(CategoryFilter),
-    recommend(User, MinPrice, MaxPrice, CategoryFilter, Recommendations),
-    nl,
-    write('Recommendations for '), write(User), write(':'), nl,
-    print_recommendations(Recommendations).
+    (   search_history(User, _)    % Check if the user exists in the search history
+    ->  (
+            write('Enter the minimum price (or type "skip" to skip): '),
+            read(MinPriceInput),
+            parse_price_input(MinPriceInput, MinPrice),
+            write('Enter the maximum price (or type "skip" to skip): '),
+            read(MaxPriceInput),
+            parse_price_input(MaxPriceInput, MaxPrice),
+            write('Enter the category (or type "skip" to skip): '),
+            read(CategoryFilter),
+            recommend(User, MinPrice, MaxPrice, CategoryFilter, Recommendations),
+            nl,
+            write('Recommendations for '), write(User), write(':'), nl,
+            print_recommendations(Recommendations)
+        )
+    ;   write('User not found.')
+    ).
 
+% Parse the price input, converting "skip" to -1.0
 parse_price_input(skip, -1.0) :- !.
 parse_price_input(Value, ParsedValue) :-
     number(Value),
     ParsedValue is float(Value).
 
+% Print the list of recommendations
 print_recommendations([]) :-
     nl,
     write('No suggestions.'), nl.
 print_recommendations(Recommendations) :-
     print_recommendations_list(Recommendations).
 
+% Print each recommendation in the list
 print_recommendations_list([]).
 print_recommendations_list([Recommendation|Tail]) :-
     write('- '), write(Recommendation), nl,
